2ca5e5dc2d5f32c3237d187ddc4198348b786455
Berlinr <nickaf2020@gmail.com>
2023-09-04 00:07:07 +0300

    new index page and nav.
    new index page and nav.

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const submenuLinks = document.querySelectorAll(".submenu-trigger");
+    const submenus = document.querySelectorAll(".submenu");
+
+    // Function to close all submenus
+    function closeAllSubmenus() {
+        submenus.forEach((submenu) => {
+            submenu.style.display = "none";
+        });
+    }
+
+    submenuLinks.forEach((link) => {
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+            const submenu = link.nextElementSibling;
+            const isSubMenuVisible = submenu.style.display === "block";
+
+            // Close all submenus
+            closeAllSubmenus();
+
+            // Show or hide the clicked submenu
+            submenu.style.display = isSubMenuVisible ? "none" : "block";
+        });
+
+        link.addEventListener("mouseover", function () {
+            const submenu = link.nextElementSibling;
+            submenu.style.display = "block";
+        });
+
+        // Close the submenu when the mouse leaves it
+        link.nextElementSibling.addEventListener("mouseleave", function () {
+            this.style.display = "none";
+        });
+    });
+
+    // Close submenus when clicking outside of them
+    document.addEventListener("click", function (e) {
+        if (!e.target.classList.contains("submenu-trigger")) {
+            closeAllSubmenus();
+        }
+    });
+});
+
+
+// card flipping code --->
+
 const cardContainers = document.querySelectorAll('.card-container');
 
 // Your existing card flipping code
@@ -33,7 +79,8 @@ cardContainers.forEach(container => {
 // Toggle button for dark mode2 
 const toggleButton2 = document.getElementById('switch');
 const stylesheetLink2 = document.getElementById('stylesheet-link');
-let isDarkMode2 = false;
+let isDarkMode2 = true;
+toggleButton2.checked = true;
 
 toggleButton2.addEventListener('click', () => {
   isDarkMode2 = !isDarkMode2;

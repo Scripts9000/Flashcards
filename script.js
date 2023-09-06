eb05e4406cee086de1214495047fa9f902b05642
Berlinr <nickaf2020@gmail.com>
2023-09-06 22:05:31 +0300

    Some updates to the nav menu
    and a bubble when pointing at the card
    additional pages and some emojy in html

--- script.js
+++ script.js
@@ -1,92 +1,107 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const submenuLinks = document.querySelectorAll(".submenu-trigger");
-    const submenus = document.querySelectorAll(".submenu");
+  const submenuLinks = document.querySelectorAll(".submenu-trigger");
+  const submenus = document.querySelectorAll(".submenu");
 
-    // Function to close all submenus
-    function closeAllSubmenus() {
-        submenus.forEach((submenu) => {
-            submenu.style.display = "none";
-        });
-    }
+  // Function to close all submenus
+  function closeAllSubmenus() {
+    submenus.forEach((submenu) => {
+      submenu.style.display = "none";
+    });
+  }
 
-    submenuLinks.forEach((link) => {
-        link.addEventListener("click", function (e) {
-            e.preventDefault();
-            const submenu = link.nextElementSibling;
-            const isSubMenuVisible = submenu.style.display === "block";
+  submenuLinks.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const submenu = link.nextElementSibling;
+      const isSubMenuVisible = submenu.style.display === "block";
 
-            // Close all submenus
-            closeAllSubmenus();
+      // Close all submenus
+      closeAllSubmenus();
 
-            // Show or hide the clicked submenu
-            submenu.style.display = isSubMenuVisible ? "none" : "block";
-        });
-
-        link.addEventListener("mouseover", function () {
-            const submenu = link.nextElementSibling;
-            submenu.style.display = "block";
-        });
-
-        // Close the submenu when the mouse leaves it
-        link.nextElementSibling.addEventListener("mouseleave", function () {
-            this.style.display = "none";
-        });
+      // Show or hide the clicked submenu
+      submenu.style.display = isSubMenuVisible ? "none" : "block";
     });
 
-    // Close submenus when clicking outside of them
-    document.addEventListener("click", function (e) {
-        if (!e.target.classList.contains("submenu-trigger")) {
-            closeAllSubmenus();
-        }
+    link.addEventListener("mouseover", function () {
+      const submenu = link.nextElementSibling;
+      submenu.style.display = "block";
     });
-});
 
+    // Close the submenu when the mouse leaves it
+    link.nextElementSibling.addEventListener("mouseleave", function () {
+      this.style.display = "none";
+    });
+  });
 
-// card flipping code --->
+  // Card flipping code
+  const cardContainers = document.querySelectorAll('.card-container');
 
-const cardContainers = document.querySelectorAll('.card-container');
+  cardContainers.forEach(container => {
+    const cards = container.querySelectorAll('.card');
+    cards.forEach(card => {
+      const cardBack = card.querySelector('.card-back');
+      let isFlipped = false;
 
-// Your existing card flipping code
-cardContainers.forEach(container => {
-  const cards = container.querySelectorAll('.card');
-  cards.forEach(card => {
-    const cardBack = card.querySelector('.card-back');
-    let isFlipped = false;
-
-    card.addEventListener('click', () => {
-      if (!isFlipped) {
-        card.classList.add('flipped');
-        cardBack.style.display = 'block';
-      } else {
-        card.classList.remove('flipped');
-        cardBack.style.display = 'none';
-      }
-
-      isFlipped = !isFlipped;
-
-      // Add the automatic flip back after 5 seconds
-      if (isFlipped) {
-        setTimeout(() => {
+      card.addEventListener('click', () => {
+        if (!isFlipped) {
+          card.classList.add('flipped');
+          cardBack.style.display = 'block';
+        } else {
           card.classList.remove('flipped');
           cardBack.style.display = 'none';
-          isFlipped = false;
-        }, 3000); // 5000 milliseconds = 5 seconds
+        }
+
+        isFlipped = !isFlipped;
+
+        // Add automatic flip back after 5 seconds
+        if (isFlipped) {
+          setTimeout(() => {
+            card.classList.remove('flipped');
+            cardBack.style.display = 'none';
+            isFlipped = false;
+          }, 5000); // 5000 milliseconds = 5 seconds
+        }
+      });
+    });
+  });
+
+  // Toggle button for dark mode
+  const toggleButton = document.getElementById('switch');
+  const stylesheetLink = document.getElementById('stylesheet-link');
+  let isDarkMode = true;
+  toggleButton.checked = true;
+
+  toggleButton.addEventListener('click', () => {
+    isDarkMode = !isDarkMode;
+    if (isDarkMode) {
+      stylesheetLink.href = 'dark-styles.css';
+    } else {
+      stylesheetLink.href = 'styles.css';
+    }
+  });
+
+  // Info functionality
+  const cards = document.querySelectorAll('.card');
+  let popupTimeout;
+
+  cards.forEach(card => {
+    const popup = card.querySelector('.popup');
+    
+    card.addEventListener('mouseenter', () => {
+      // Set a timeout to display the popup after 2 seconds
+      popupTimeout = setTimeout(() => {
+        popup.style.display = 'block';
+      }, 1000); // 1000 milliseconds = 1 seconds
+    });
+
+    // Add mouseout event listener to hide the pop-up when the user moves away from the card
+    card.addEventListener('mouseout', (e) => {
+      // Check if the mouse is leaving the card to enter the pop-up
+      if (!e.relatedTarget || !popup.contains(e.relatedTarget)) {
+        // Hide the pop-up
+        clearTimeout(popupTimeout);
+        popup.style.display = 'none';
       }
     });
   });
-});
-
-// Toggle button for dark mode2 
-const toggleButton2 = document.getElementById('switch');
-const stylesheetLink2 = document.getElementById('stylesheet-link');
-let isDarkMode2 = true;
-toggleButton2.checked = true;
-
-toggleButton2.addEventListener('click', () => {
-  isDarkMode2 = !isDarkMode2;
-  if (isDarkMode2) {
-    stylesheetLink2.href = 'dark-styles.css';
-  } else {
-    stylesheetLink2.href = 'styles.css';
-  }
 });

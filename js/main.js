ae73b7c5b522fb81005c4a3d88f424171f104a1d
Berlinr <nickaf2020@gmail.com>
2023-11-12 23:44:53 +0100

    kofi update

--- js/main.js
+++ js/main.js
@@ -62,7 +62,7 @@ class MyHeader extends HTMLElement {
                     <li class="dropdown">
                         <a href="donate.html" class="submenu-trigger"><i class="fa fa-heart fa-lg" aria-hidden="true"></i></a>
                         <ul class="submenu">
-                        <li><a href="ko-fi.html"><i class="fa fa-arrow-right fa-lg" aria-hidden="true"></i> Ko-Fi - <i class="fa fa-btc fa-lg" aria-hidden="true"></i></a><li>
+                        <li><a href='https://ko-fi.com/L3L8R1QN8' target='_blank'><img height='30' style='border:0px;height:30px;' src='https://storage.ko-fi.com/cdn/kofi2.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a></li>
                         <a href="donate_crypto.html"><i class="fa fa-arrow-right fa-lg" aria-hidden="true"></i> Bitcoin - <i class="fa fa-btc fa-lg" aria-hidden="true"></i></a>
                         </ul>
                     </li>

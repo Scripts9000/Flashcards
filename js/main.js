6973cc4a10c5d7530cd07544f1c8f60c8701aad6
Berlinr <nickaf2020@gmail.com>
2023-11-13 00:06:15 +0100

    ss

--- js/main.js
+++ js/main.js
@@ -66,7 +66,7 @@ class MyHeader extends HTMLElement {
                         </ul>
                     </li>
                     <li>
-                    <a href='https://ko-fi.com/L3L8R1QN8' target='_blank'><img height='36' style='border:0px;height:36px;' src='https://storage.ko-fi.com/cdn/kofi2.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
+                    <a href='https://ko-fi.com/L3L8R1QN8' target='_blank'><img height='30' style='border:0px;height:30px;' src='https://storage.ko-fi.com/cdn/kofi2.png?v=3' border='0' alt='Buy Me a Coffee at ko-fi.com' /></a>
                     </li>
                 </ul>
             </nav>

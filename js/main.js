a1b9ed30c393b7986c0713724477ce6baee3db3a
Berlinr <nickaf2020@gmail.com>
2023-12-10 04:15:28 +0100

    sd

--- js/main.js
+++ js/main.js
@@ -84,7 +84,7 @@ class MyFooter extends HTMLElement {
         <footer>
             <address>
                 © Hebrewy.org<br/>
-                <a href="https://hebrewy.org/ko-fi.html">Buy me a Coffee</a>
+                <a href="https://hebrewy.org/ko-fi.html">Donate</a>
             </address>
         </footer>
         `

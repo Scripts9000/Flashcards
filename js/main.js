dd8bb574da3fed2329d4210854d3c81e68e83b35
Berlinr <nickaf2020@gmail.com>
2024-01-29 22:56:16 +0100

    coffee update

--- js/main.js
+++ js/main.js
@@ -65,7 +65,7 @@ class MyHeader extends HTMLElement {
                     </li>
                     
 
-                    <li><a href="#"><i class="fa fa-gears fa-lg" aria-hidden="true"></i> Register</a></li>
+                    <li><a href="https://ko-fi.com/hebrewy"><img src="/img/coffee-cup.png" alt="Buy me A Coffee"></a></li>
                     <!--<li class="dropdown">
                         <a href="donate.html" class="submenu-trigger"><i class="fa fa-heart fa-lg" aria-hidden="true"></i></a>
                         <ul class="submenu">

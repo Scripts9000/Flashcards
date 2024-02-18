8d48f6c0071c87b74daac753c83713294277e373
Berlinr <nickaf2020@gmail.com>
2024-02-18 12:09:43 +0100

    corrections

--- js/script.js
+++ js/script.js
@@ -95,42 +95,5 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
-
-  // Toggle button for dark mode
-  const toggleButton = document.getElementById('switch');
-  const stylesheetLink = document.getElementById('stylesheet-link');
-  let isDarkMode = true;
-
-
-  // Info functionality
-  const cards = document.querySelectorAll('.card');
-  let popupTimeout;
-
-  cards.forEach(card => {
-    const popup = card.querySelector('.popup');
-    
-    card.addEventListener('mouseenter', () => {
-      // Set a timeout to display the popup after 2 seconds
-      popupTimeout = setTimeout(() => {
-        popup.style.display = 'block';
-      }, 500); // 500 milliseconds = 0.5 seconds
-    });
-
-    // Add mouseout event listener to hide the pop-up when the user moves away from the card
-    card.addEventListener('mouseout', (e) => {
-      // Check if the mouse is leaving the card to enter the pop-up
-      if (!e.relatedTarget || !popup.contains(e.relatedTarget)) {
-        // Hide the pop-up
-        clearTimeout(popupTimeout);
-        popup.style.display = 'none';
-      }
-    });
-  });
-
-  
-  
-  
-
-
 });
 
